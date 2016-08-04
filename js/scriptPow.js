ebf6f98b211be1ebf2142566f0ba495479cea005
Lyamov Sergey <sirozhechka@mail.ru>
2016-08-04 18:50:30 +0300

    fixed scriptPow.js

--- js/scriptPow.js
+++ js/scriptPow.js
@@ -1,34 +1,32 @@
+//№1
 
-
-function pow(a, b){
-	var result;
-	result = Math.pow(a, b)
-	console.log('answer =', result);
+/*function pow(a, b) {
+	var result = a;
+ for (var i = 1; i < b; i++){
+	 result = result * a;
+ }
 	alert('Ответ в консоли');
+	
+	console.log('answer =', result);
+}
+pow( prompt('Введите число', 1), prompt('Введите степень', 1) );*/
+
+
+
+// или №2
+
+
+
+function pow(a, b) {
+	var result = a;
+	var i = 1;
+  while(i < b){
+	  result = result * a;
+	  i++;
+  }	
+	alert('Ответ в консоли');
+	
+	console.log('answer =', result);
 }
 pow( prompt('Введите число', 1), prompt('Введите степень', 1) );
 
-/*var names = [];
-
-for( var i = 0; i < 5; i++){
-	var result = prompt('add name', '');
-	names.push(result);
-	
-}
-
-var userName = prompt('Введите имя пользователя', '');
-
-if(userName == names[0]){
-	alert(userName + ', вы успешно вошли!');
-}else if(userName == names[1]){
-	alert(userName + ', вы успешно вошли!');
-}else if(userName == names[2]){
-	alert(userName + ', вы успешно вошли!');
-}else if(userName == names[3]){
-	alert(userName + ', вы успешно вошли!');
-}else if(userName == names[4]){
-	alert(userName + ', вы успешно вошли!');
-}else{
-	alert('!!!FATAL ERROR!!!! Your computer will be destroyed')
-}
-*/
